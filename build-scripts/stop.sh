70454ec9cd0f3ae409f44845d945cd3e7be0e567
usuallymarcel <marcelve2112@gmail.com>
2025-12-20 20:19:25 +1300

    remove image too

--- build-scripts/stop.sh
+++ build-scripts/stop.sh
@@ -1,9 +1,15 @@
 CONTAINER=fastapi
+IMAGE=nixos-server-fastapi
 
 if [[ -n $1 ]]; then
 	CONTAINER=$1
 fi	
-echo $CONTAINER		
+if [[ -n $2 ]]; then
+	IMAGE=$2
+fi
+echo $CONTAINER	
+echo $IMAGE
 
 docker stop $CONTAINER && docker rm $CONTAINER
 
+docker rmi $IMAGE
